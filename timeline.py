3131a61c1a704e2dcce9cd191497956fc6170e33
Matthew Trotter <matthew.trotter1@gmail.com>
2021-05-24 15:17:44 -0500

    detects errors

--- timeline.py
+++ timeline.py
@@ -1,15 +1,14 @@
 import argparse 
 import datetime
+from itertools import repeat
 import json
 import logging
 from multi_key_dict import multi_key_dict
+from multiprocessing import Pool, Value
 import pandas as pd
 from pathlib import Path
 import re
 from typing import Any, List
-
-logger = logging.getLogger('TIMELINE')
-logger.setLevel(logging.INFO)
 
 class GatewayLogIngestor:
     gw_id: int
@@ -17,7 +16,7 @@ class GatewayLogIngestor:
     
     def __init__(self, gw_log: Path) -> None:
         self.gw_id = int(str(gw_log.stem).split('-')[1])       # extracts the integer 0 from ajs_station-0.log filename
-        logger.info(f'Ingesting {gw_log} ...')
+        print(f'Ingesting {gw_log} ...')
 
         # Import file
         self.log = pd.read_csv(
@@ -30,37 +29,64 @@ class GatewayLogIngestor:
         
         # Ignore unimportant rows
         self.log = self.log[~self.log.Data.str.contains("GPS -- time gps:")]
+        self.log = self.log[~self.log.Data.str.contains("A transmission is already scheduled (!overflow error!)")]
+        self.log = self.log[~self.log.Data.str.contains("Aborting TX was OK")]
         self.log = self.log[~self.log.Data.str.contains("STATS: max gps timeref")]
         self.log = self.log[~self.log.Data.str.contains("checking prev airtime")]
         self.log = self.log[~self.log.Data.str.contains("checking next airtime")]
         self.log = self.log[~self.log.Data.str.contains("We received a PONG")]
-        # self.log = self.log[~self.log.Data.str.contains("Missed a transmission by")]
+        self.log = self.log[~self.log.Data.str.contains("beacon is being sent on PPM")]
 
         # Set index to timestamp
         self.log['Timestamp'] = self.log.iloc[:,0].str.extract('(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3})', expand=False)
         self.log['Timestamp'] = pd.to_datetime(self.log['Timestamp'], format='%Y-%m-%d %H:%M:%S.%f')
+        self.log['Timestamp'] = self.log['Timestamp'] - pd.DateOffset(hours=8)
         self.log.set_index('Timestamp')
 
         # Extract data
-        self.log[['Event', 'FCnt', 'DevEui', 'DevAddr', 'pdu']] = None
-        self.log[['Event', 'FCnt', 'DevEui', 'DevAddr', 'pdu']] = self.log['Data'].apply(self._parse_data_string)
+        self.log[['Event', 'FCnt', 'DevEui', 'DevAddr', 'pdu', 'seqno']] = None
+        self.log[['Event', 'FCnt', 'DevEui', 'DevAddr', 'pdu', 'seqno']] = self.log['Data'].apply(self._parse_data_string)
         self.log.drop(columns=['Data'])
 
     def _parse_data_string(self, data: str):
         """Parse single line from log file
         """
-        type = data.split(': ')[0]
-        if type == 'UP':
+        if 'UP: ' in data:
             data = json.loads(data.split('UP: ')[1])
+            msgtype = data.get('msgtype', None)
+            if msgtype:
+                msgtype = msgtype.upper()
             return pd.Series([
-                data.get('msgtype', None),
+                msgtype,
                 data.get('FCnt', None),
                 data.get('DevEui', None),
                 data.get('DevAddr', None),
                 data.get('pdu', None),
+                None
             ])
-        elif type == 'DN':
-            pass
+        elif 'DN: [On-Air] ' in data:
+            data = json.loads(data.split('DN: [On-Air] ')[1])
+            return pd.Series([
+                'DN: [On-Air]',
+                None,
+                data.get('DevEui', None),
+                None,
+                None,
+                data.get('seqno', None),
+            ])
+        elif 'DN: [Scheduled] ' in data:
+            pdu = re.search('[0-9a-fA-F]{14,}', data).group()
+            event = 'DN: [Scheduled] MAC'
+            if pdu[:2] == '20':
+                event = 'DN: [Scheduled] JACC'
+            return pd.Series([
+                event,
+                None,
+                None,
+                None,
+                pdu,
+                None,
+            ])
 
         return pd.Series([
             'UNKNOWN',
@@ -75,7 +101,7 @@ class JoinsLogIngestor:
     log = pd.DataFrame()
     
     def __init__(self, joins_log: Path) -> None:
-        logger.info(f'Ingesting {joins_log} ...')
+        print(f'Ingesting {joins_log} ...')
 
         # Import file
         self.log = pd.read_csv(
@@ -126,7 +152,7 @@ class NwksLogIngestor:
     log = pd.DataFrame()
     
     def __init__(self, nwks_log: Path) -> None:
-        logger.info(f'Ingesting {nwks_log} ...')
+        print(f'Ingesting {nwks_log} ...')
 
         # Import file
         self.log = pd.read_csv(
@@ -219,7 +245,7 @@ class DoorLogIngestor:
     log = pd.DataFrame()
     
     def __init__(self, door_log: Path) -> None:
-        logger.info(f'Ingesting {door_log} ...')
+        print(f'Ingesting {door_log} ...')
 
         # Import file
         self.log = pd.read_csv(
@@ -243,7 +269,6 @@ class DoorLogIngestor:
     def _parse_data_string(self, data: str):
         """Parse single line from log file
         """
-        deveui = re.search('([0-9A-F]{2}[:-]){7}[0-9A-F]{2}', data).group()
         if 'SENDING Muxs...' in data:
             data = data.replace("\'", "\"")
             data = eval(data.split('SENDING Muxs... ')[1])
@@ -254,9 +279,10 @@ class DoorLogIngestor:
                 data.get('diid', None),
             ])
 
+        deveui = re.search('([0-9A-F]{2}[:-]){7}[0-9A-F]{2}', data).group()
         return pd.Series([
             'UNKNOWN',
-            None,
+            deveui,
             None,
             None
         ])
@@ -267,7 +293,7 @@ class MuxsLogIngestor:
     log = pd.DataFrame()
     
     def __init__(self, muxs_log: Path) -> None:
-        logger.info(f'Ingesting {muxs_log} ...')
+        print(f'Ingesting {muxs_log} ...')
 
         # Import file
         self.log = pd.read_csv(
@@ -293,7 +319,10 @@ class MuxsLogIngestor:
         """
         if data:
             if 'Unknown DevAddr' in data:
-                devaddr = int(re.search('\d{5,}/0x', data).group().split('/')[0])
+                result = re.search('\d{5,}/0x', data)
+                devaddr = None
+                if result:
+                    devaddr = int(result.group().split('/')[0])
                 return pd.Series([
                     'Unknown DevAddr',
                     devaddr
@@ -310,6 +339,7 @@ class DeviceTimeline:
     deveui: str
     devaddrs = list()
     diids = list()
+    pdus = list()
     start_time: datetime.datetime
 
     def __init__(self, deveui: str, start_time: datetime.datetime) -> None:
@@ -326,11 +356,14 @@ class DeviceTimeline:
             ])
         self.deveui = deveui
         self.start_time = start_time
+        self.output_dir = Path('./output')
+        self.output_dir.mkdir(exist_ok=True)
 
     def __str__(self) -> str:
         return self.deveui
 
     def extract(self, ingestors: List[Any]) -> None:
+        # Extract data based on DevEui
         for ingestor in ingestors:
             if isinstance(ingestor, GatewayLogIngestor):
                 self._extract_from_gateway(ingestor)
@@ -338,8 +371,20 @@ class DeviceTimeline:
                 self._extract_from_joins(ingestor)
             if isinstance(ingestor, NwksLogIngestor):
                 self._extract_from_nwks(ingestor)
+            if isinstance(ingestor, DoorLogIngestor):
+                self._extract_from_door(ingestor)
             if isinstance(ingestor, MuxsLogIngestor):
                 self._extract_from_muxs(ingestor)
+
+        # Extract based on metadata associated with this device
+        for ingestor in ingestors:
+            if isinstance(ingestor, GatewayLogIngestor):
+                self._extract_from_gateway_meta(ingestor)
+            if isinstance(ingestor, MuxsLogIngestor):
+                self._extract_from_muxs_meta(ingestor)
+
+        self.cleanup_timeline()
+
 
     def _extract_from_gateway(self, gw_ingestor: GatewayLogIngestor):
         new_col = f'GW{gw_ingestor.gw_id}'
@@ -347,8 +392,8 @@ class DeviceTimeline:
         relevant.loc[:,new_col] = relevant['Event']
         self.timeline[new_col] = None
         self.timeline = pd.concat([self.timeline, relevant[['Timestamp', new_col]]], join='outer')
-        self.timeline = self.timeline.sort_values(by='Timestamp', axis=0)
-        self.timeline = self.timeline.reset_index(drop=True)
+        self.cleanup_timeline()
+
 
     def _extract_from_joins(self, joins_ingestor: JoinsLogIngestor):
         new_col = 'JOINS'
@@ -356,8 +401,8 @@ class DeviceTimeline:
         relevant.loc[:,new_col] = relevant['Event']
         self.timeline[new_col] = None
         self.timeline = pd.concat([self.timeline, relevant[['Timestamp', new_col]]], join='outer')
-        self.timeline = self.timeline.sort_values(by='Timestamp', axis=0)
-        self.timeline = self.timeline.reset_index(drop=True)
+        self.cleanup_timeline()
+
 
     def _extract_from_nwks(self, nwks_ingestor: JoinsLogIngestor):
         new_col = 'NWKS'
@@ -365,8 +410,7 @@ class DeviceTimeline:
         relevant.loc[:,new_col] = relevant['Event']
         self.timeline[new_col] = None
         self.timeline = pd.concat([self.timeline, relevant[['Timestamp', new_col]]], join='outer')
-        self.timeline = self.timeline.sort_values(by='Timestamp', axis=0)
-        self.timeline = self.timeline.reset_index(drop=True)
+        self.cleanup_timeline()
 
         # Get associated devaddrs and diids
         self.devaddrs += relevant.loc[relevant['DevAddr'].notna(), 'DevAddr'].to_list()
@@ -380,81 +424,160 @@ class DeviceTimeline:
         relevant.loc[:,new_col] = relevant['Event']
         self.timeline[new_col] = None
         self.timeline = pd.concat([self.timeline, relevant[['Timestamp', new_col]]], join='outer')
-        self.timeline = self.timeline.sort_values(by='Timestamp', axis=0)
-        self.timeline = self.timeline.reset_index(drop=True)
+        self.cleanup_timeline()
 
         # Get associated pdus and diids
         self.pdus += relevant.loc[relevant['pdu'].notna(), 'pdu'].to_list()
         self.diids += relevant.loc[relevant['diid'].notna(), 'diid'].to_list()
 
+
     def _extract_from_muxs(self, muxs_ingestor: MuxsLogIngestor):
         new_col = 'MUXS'
-        checks = [muxs_ingestor.log['DevAddr'] == devaddr for devaddr in self.devaddrs]
-        found = muxs_ingestor.log['DevAddr'] > -1e15
-        for check in checks:
-            found |= check
+        self.timeline[new_col] = None
+        if self.devaddrs:
+            checks = [muxs_ingestor.log['DevAddr'] == devaddr for devaddr in self.devaddrs]
+            found = muxs_ingestor.log['DevAddr'] > -1e15
+            for check in checks:
+                found |= check
+            relevant = muxs_ingestor.log.loc[found]
+            relevant.loc[:,new_col] = relevant['Event']
+            self.timeline = pd.concat([self.timeline, relevant[['Timestamp', new_col]]], join='outer')
+            self.cleanup_timeline()
+
+
+    def _extract_from_gateway_meta(self, gw_ingestor: GatewayLogIngestor):
+        new_col = f'GW{gw_ingestor.gw_id}'
+        found = gw_ingestor.log['DevAddr'].isin(self.devaddrs) | \
+                gw_ingestor.log['pdu'].isin(self.pdus)# | \
+                # gw_ingestor.log['seqno'].isin(self.diids)     # don't need to do this - already captured by deveui
+        relevant = gw_ingestor.log.loc[found]
+        relevant.loc[:,new_col] = relevant['Event']
+        self.timeline = pd.concat([self.timeline, relevant[['Timestamp', new_col]]], join='outer')
+        self.cleanup_timeline()
+
+
+    def _extract_from_muxs_meta(self, muxs_ingestor: MuxsLogIngestor):
+        new_col = 'MUXS'
+        found = muxs_ingestor.log['DevAddr'].isin(self.devaddrs)
         relevant = muxs_ingestor.log.loc[found]
         relevant.loc[:,new_col] = relevant['Event']
-        self.timeline[new_col] = None
         self.timeline = pd.concat([self.timeline, relevant[['Timestamp', new_col]]], join='outer')
-        self.timeline = self.timeline.sort_values(by='Timestamp', axis=0)
-        self.timeline = self.timeline.reset_index(drop=True)
+        self.cleanup_timeline()
 
-        print()
-
-    def print(self):
-        """Print the timeline to screen
-        """
-        pass
 
     def check_for_errors(self):
         """Analyze the sequence of events for operational errors
         """
-        pass
+        normal_join_note = 'Normal join behavior for LNS'
+        device_not_joined_note = 'Device Error: Device tried to join again after successful join process.'
+        gw_missed_dn_jacc = 'LNS Error: Gateway scheduled JACC but did not transmit it.'
 
+        self.timeline['Notes'] = ''
+        for i in range(1, self.timeline.shape[0]+1):
+            if i == 3:
+                if self._normal_join_process(self.timeline.loc[(i-3):i]):
+                    self.timeline.loc[i,'Notes'] = normal_join_note
+            if i > 3:
+                if self._normal_join_process(self.timeline.loc[(i-4):i]):
+                    self.timeline.loc[i,'Notes'] = normal_join_note
+            if self.timeline.loc[i-1,'Notes'] == normal_join_note:
+                if self._device_not_joined(self.timeline.loc[(i-1):i]):
+                    self.timeline.loc[i,'Notes'] = device_not_joined_note
+            if i < self.timeline.shape[0]:
+                if self._missing_on_air(self.timeline.loc[(i-1):i]):
+                    self.timeline.loc[i,'Notes'] = gw_missed_dn_jacc
+
+
+    def _normal_join_process(self, tl: pd.DataFrame):
+        """Returns true if the timeline shows correct join behavior from the LNS point of view.
+
+        Parameters
+        ----------
+        tl : pd.DataFrame
+            Timeline of events, length 4 or 5
+        """
+        gw_got_jreq = (tl.iloc[-4]['GW0'] == 'JREQ') or (tl.iloc[-4]['GW1'] == 'JREQ')
+        if tl.shape[0] > 4:
+            gw_got_jreq = gw_got_jreq or (tl.iloc[-5]['GW0'] == 'JREQ') or (tl.iloc[-5]['GW1'] == 'JREQ')
+        door_sending_muxs = tl.iloc[-3]['DOOR'] == 'SENDING Muxs...'
+        gw_sent_jacc =  (tl.iloc[-2]['GW0'] == 'DN: [Scheduled] JACC' and tl.iloc[-1]['GW0'] == 'DN: [On-Air]') or \
+                        (tl.iloc[-2]['GW1'] == 'DN: [Scheduled] JACC' and tl.iloc[-1]['GW1'] == 'DN: [On-Air]')
+        return gw_got_jreq and door_sending_muxs and gw_sent_jacc
+        
+
+    def _device_not_joined(self, tl: pd.DataFrame):
+        """Returns true if device sends new JREQ after LNS sends JACC
+
+        Parameters
+        ----------
+        tl : pd.DataFrame
+            Timeline of events, length 2
+        """
+        return  ((tl.iloc[0]['GW0'] == 'DN: [On-Air]') or (tl.iloc[0]['GW1'] == 'DN: [On-Air]')) and \
+                ((tl.iloc[1]['GW0'] == 'JREQ') or (tl.iloc[1]['GW1'] == 'JREQ'))
+
+
+    def _missing_on_air(self, tl: pd.DataFrame):
+        """Returns true if gateway schedules a transmission but doesn't send it
+
+        Parameters
+        ----------
+        tl : pd.DataFrame
+            Timeline of events, length 2
+        """
+        return  ((tl.iloc[1]['GW0'] != 'DN: [On-Air]') and (tl.iloc[0]['GW0'] == 'DN: [Scheduled] JACC')) or \
+                ((tl.iloc[1]['GW1'] != 'DN: [On-Air]') and (tl.iloc[0]['GW1'] == 'DN: [Scheduled] JACC'))
+
+    def cleanup_timeline(self):
+        """Print the timeline to screen
+        """
+        self.timeline = self.timeline.fillna('')        # Remove all None and NaN
+        self.timeline = self.timeline.sort_values(by='Timestamp', axis=0)
+        self.timeline = self.timeline.reset_index(drop=True)
+
+
+    def to_xlsx(self):
+        filename = self.output_dir / f'{self.deveui}.xlsx'
+        self.cleanup_timeline()
+        self.timeline.to_excel(filename)
+
+
+def ingest(logtype: str, logfolder: Path):
+    """Ingest a log file of a certain type (station, joins, nwks, ...)
+
+    Parameters
+    ----------
+    type : str
+        one of ['*station-0.log', '*station-1.log', '*joins.log', '*nwks.log', '*door.log', '*muxs.log']
+    """
+    allowed_types = ['*station-0.log', '*station-1.log', '*joins.log', '*nwks.log', '*door.log', '*muxs.log']
+    if logtype not in allowed_types:
+        raise ValueError('Invalid log type')
+    logs = [f for f in logfolder.glob(logtype)]
+    if len(logs) > 1:
+        raise FileExistsError(f'There should only be one *station-0.log file. Instead found: {logs}')
+    if len(logs) == 1:
+        if logtype == allowed_types[0] or logtype == allowed_types[1]:
+            return GatewayLogIngestor(logs[0])
+        if logtype == allowed_types[2]:
+            return JoinsLogIngestor(logs[0])
+        if logtype == allowed_types[3]:
+            return NwksLogIngestor(logs[0])
+        if logtype == allowed_types[4]:
+            return DoorLogIngestor(logs[0])
+        if logtype == allowed_types[5]:
+            return MuxsLogIngestor(logs[0])
+    return None
 
 def main(args):
-    ingestors = []
-
-    # Gateway ingestors
-    gw0_logs = [f for f in args.logfolder.glob('*station-0.log')]
-    if len(gw0_logs) > 1:
-        raise FileExistsError(f'There should only be one *station-0.log file. Instead found: {gw0_logs}')
-    gw0_ingestor = GatewayLogIngestor(gw0_logs[0])
-    ingestors.append(gw0_ingestor)
-    gw1_logs = [f for f in args.logfolder.glob('*station-1.log')]
-    if len(gw1_logs) > 1:
-        raise FileExistsError(f'There should only be one *station-1.log file. Instead found: {gw1_logs}')
-    gw1_ingestor = GatewayLogIngestor(gw1_logs[0])
-    ingestors.append(gw1_ingestor)
-
-    # Joins ingestor
-    joins_logs = [f for f in args.logfolder.glob('*joins.log')]
-    if len(joins_logs) > 1:
-        raise FileExistsError(f'There should only be one *joins.log file. Instead found: {joins_logs}')
-    joins_ingestor = JoinsLogIngestor(joins_logs[0])
-    ingestors.append(joins_ingestor)
-
-    # Nwks ingestor
-    nwks_logs = [f for f in args.logfolder.glob('*nwks.log')]
-    if len(nwks_logs) > 1:
-        raise FileExistsError(f'There should only be one *nwks.log file. Instead found: {nwks_logs}')
-    nwks_ingestor = NwksLogIngestor(nwks_logs[0])
-    ingestors.append(nwks_ingestor)
-
-    # Door ingestor
-    door_logs = [f for f in args.logfolder.glob('*door.log')]
-    if len(nwks_logs) > 1:
-        raise FileExistsError(f'There should only be one *door.log file. Instead found: {door_logs}')
-    door_ingestor = DoorLogIngestor(door_logs[0])
-    ingestors.append(door_ingestor)
-
-    # Muxs ingestor
-    muxs_logs = [f for f in args.logfolder.glob('*muxs.log')]
-    if len(muxs_logs) > 1:
-        raise FileExistsError(f'There should only be one *muxs.log file. Instead found: {muxs_logs}')
-    muxs_ingestor = MuxsLogIngestor(muxs_logs[0])
-    ingestors.append(muxs_ingestor)
+    correctlogfiles = ['*station-0.log', '*station-1.log', '*joins.log', '*nwks.log', '*door.log', '*muxs.log']
+    
+    # Multi-processed version
+    with Pool() as p:
+        ingestors = p.starmap(ingest, zip(correctlogfiles, repeat(args.logfolder)))
+    
+    # Single-processed version
+    # ingestors = [ingest(correctlogfile, args.logfolder) for correctlogfile in correctlogfiles]
 
     # Parse specified devices
     devices = []
@@ -468,11 +591,11 @@ def main(args):
     
     for device in devices:
         device.extract(ingestors)
-    print()
+        device.check_for_errors()
+        device.to_xlsx()
     
 
 if __name__ == "__main__":
-    logger.info('asdf')
     parser = argparse.ArgumentParser(
         prog='timeline',
         description='Analyze device behavior from LNS logs.',
